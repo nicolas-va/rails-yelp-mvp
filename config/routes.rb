c28330c3561369f42fe1156e7266303482f464ce
nicolas vassel <nicolas.vassel@gmail.com>
2021-02-17 16:22:24 +0000

    review controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :restaurants
+
+  #create a route review
+  resources :review, only: [:new, :create]
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
